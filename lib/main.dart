6d77caa635b4dad5801a17ae67d86c9118c611ea
Mohak Makin <mohakmakin721@gmail.com>
2020-12-17 18:58:52 +0530

    renamed flutter demo to flutter

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'grow up',
+      title: 'grow up chuju',
       theme: ThemeData(
         // This is the theme of your application.
         //
