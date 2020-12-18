f24a75937b37460fb8762a22d12c85ff6126d486
vrmnyash <vrmn.yashv@gmail.com>
2020-12-17 18:33:16 -0800

    vrmn update

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'push nai ho rha',
+      title: 'push  nai ho rha',
       theme: ThemeData(
         // This is the theme of your application.
         //
